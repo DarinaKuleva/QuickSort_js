a73ccb71b41cadc69477414cb7b39524d4a1b768
Darina Kuleva <darinakuleva@gmail.com>
2019-04-07 21:26:30 +0300

    fix

--- sort.js
+++ sort.js
@@ -1,30 +1,25 @@
-let num = [24, 40, 6, 17, 45, 1, 56, 84, 5, 98, 3, 47, 89];
 
-function swap(i, j) {
-  let temp = num[i];
-  num[i] = num[j];
-  num[j] = temp;
-  ++i;
-  ++j;
-
-}
+let num  = [24, 40, 6, 17, 45, 1, 89, 84, 5, 98, 3, 47, 89];
 
 function qSort(num, min, max) {
-  let ArrayLangth = num.length;
-  let middle = ((ArrayLangth % 2) === 0) ? ArrayLangth/2 : (ArrayLangth-1)/2
+  let middle = (((min+max) % 2) === 0) ? (min+max)/2 : ((min+max)-1)/2;
   let i = min;
   let j = max;
 
-  while (i < j)  {
+  while (i <= j)  {
     while (num[i] < num[middle]) {
-     ++i;
+     i = i+1;
     }
     while (num[j] > num[middle]) {
-      --j;
+     j = j-1;
     }
 
     if (i<=j) {
-      swap(i, j)
+      let temp = num[i];
+      num[i] = num[j];
+      num[j] = temp;
+      i = i+1;
+      j = j-1;
     }
   }
 
@@ -39,12 +34,12 @@ function qSort(num, min, max) {
 }
 
 console.log (num);
-//qSort (num, 0, 12);
+qSort (num,0, 12);
 console.log (num);
 
 
 
 
 //обьединение 2 массивов
-var g = a.concat(b)
-console.log (g);
+//var g = a.concat(b)
+//console.log (g);
